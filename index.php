2ccd3c6bcc2114fa1bc2c0838df4c125f0dea899
syednuaymnadeem <syednuaymnadeem001@algomau.ca>
2025-05-21 11:43:27 +0000

    Created Login.php and created a form using html, changed the id and name and removed the values

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <h1>Assignment-1</h1>
 
 
+    <p> <a href="login.php">Click here to login</p>
+
+
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+
+<h1>Login Form</h1>
+
+
+  <form action="/validate.php">
+    <label for="username">Username</label><br>
+    <input type="text" id="username" name="username" value="John"><br>
+    <label for="password">Password</label><br>
+    <input type="text" id="password" name="password" value="Doe"><br><br>
+    <input type="submit" value="Submit">
+  </form>
+
+
+</body>
+</html>
+
